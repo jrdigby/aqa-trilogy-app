1c59534842c73ee681692b508a63abb85b9e1e9f
jrdigby <jrdigby@gmail.com>
2026-05-23 11:43:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // ====== CONFIG ======
-const SUPABASE_URL = "PASTE_YOUR_SUPABASE_URL_HERE";
-const SUPABASE_ANON_KEY = "PASTE_YOUR_SUPABASE_ANON_KEY_HERE";
+const SUPABASE_URL = "...";
+const SUPABASE_ANON_KEY = "...";
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
