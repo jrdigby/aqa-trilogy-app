60dfac5534bff0e3f21def6627678c3ca60c108c
jrdigby <jrdigby@gmail.com>
2026-06-05 21:53:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1109,7 +1109,7 @@ function markResponse(q, resp, key, markPoints) {
         } else {
           missing.push({
             ao: "AO2",
-            text: `Step 1 (Conversion) dropped: The target converted value should be ${convTarget} ${sc.conversion_label || ''}.`,
+            text: `Step 1 (Conversion) wrong: Converting ${sc.conversion_label || ''} should equal ${convTarget}.`,
             url: cleanUrl
           });
         }
